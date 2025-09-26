74effea7519a23c83ccfaafbf08e024a5bc76d02
Anton Romanov <a.romanovich86@gmail.com>
2025-09-26 13:18:09 +0300

    fix: install_postgis

--- commands/postgis.sh
+++ commands/postgis.sh
@@ -4,12 +4,10 @@ install_postgis() {(
   . "${DT_VARS}/services/$1.sh"
   LIBINTL_VERSION="0.26"
 
-  local PG_CONFIG="$(pg_bin_dir)/pg_config"
-
   if [ "$(os_name)" = "debian" ] || [ "$(os_name)" = "ubuntu" ]; then
     exec_cmd ${SUDO} apt install postgresql-${MAJOR}-postgis-3
   elif [ "$(os_kernel)" = "Darwin" ]; then
-    [ ! -d /tmp/postgis ] || ${SUDO} rm -rf /tmp/postgis
+    [ ! -d /tmp/postgis ] || exec_cmd ${SUDO} rm -rf /tmp/postgis
     if [ ! -d /opt/homebrew/Cellar/gettext/${LIBINTL_VERSION} ]; then
       dt_error ${fname} "/opt/homebrew/Cellar/gettext/${LIBINTL_VERSION} doesn't exist"
     fi
@@ -19,14 +17,21 @@ install_postgis() {(
     [ -f "postgis-${POSTGIS_VERSION}.tar.gz" ] || exec_cmd wget http://postgis.net/stuff/postgis-${POSTGIS_VERSION}.tar.gz
     exec_cmd tar -xvzf postgis-${POSTGIS_VERSION}.tar.gz -C postgis
     exec_cmd cd postgis/postgis-${POSTGIS_VERSION}
-    PG_LIBDIR=$(${PG_CONFIG} --pkglibdir | tr ' ' '\n')
-    # export LD_LIBRARY_PATH="${PG_LIBDIR}"
-    # export DYLD_LIBRARY_PATH="${PG_LIBDIR}"
+
+    local PG_CONFIG="$(pg_bin_dir)/pg_config"
+    local PG_BINDIR="$(dirname "$(${PG_CONFIG} --bindir | tr ' ' '\n')")"
+    local PG_LIBDIR="$(${PG_CONFIG} --pkglibdir | tr ' ' '\n')"
+    export CXX="/usr/bin/clang"
+    export CC="/usr/bin/clang"
+    export DYLD_LIBRARY_PATH="${PG_LIBDIR}"
+    export LD_LIBRARY_PATH="${PG_LIBDIR}"
+    export LDFLAGS="-L/opt/homebrew/Cellar/gettext/${LIBINTL_VERSION}/lib -L${PG_BINDIR}/lib"
+    export CFLAGS="-I/opt/homebrew/Cellar/gettext/${LIBINTL_VERSION}/include -I${PG_BINDIR}/include"
+    export CPPFLAGS="-I/opt/homebrew/Cellar/gettext/${LIBINTL_VERSION}/include -I${PG_BINDIR}/include"
+
     exec_cmd ./configure --with-projdir=/opt/homebrew/opt/proj --without-raster --without-protobuf \
-                --with-pgconfig="${PG_CONFIG}" \
-                "LDFLAGS=\"-L/opt/homebrew/Cellar/gettext/${LIBINTL_VERSION}/lib\"" \
-                "CFLAGS=\"-I/opt/homebrew/Cellar/gettext/${LIBINTL_VERSION}/include\""
-    # exec_cmd make PGSQL_FE_CPPFLAGS="-I${PG_LIBDIR}" PGSQL_FE_LDFLAGS="-L${PG_LIBDIR}" -lpq
+                --with-pgconfig="${PG_CONFIG}"
+    exec_cmd make
     exec_cmd make install
   else
     dt_error ${fname} "Unsupported OS: '$(os_kernel)'"; return 99
